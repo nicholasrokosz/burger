349e99448066018ec72ef870c1cf3c22998d8586
nicholasrokosz <nicholasrokosz@email.arizona.edu>
2020-12-11 20:09:15 -0700

    add controller routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,6 +2,18 @@ const burger = require('../models/burger.js');
 const express = require('express');
 const router = express.Router();
 
-// ROUTES GO HERE
+router.get('/', (req, res) =>
+  burger.selectAll(data => res.render('index', { burgers: data }))
+);
+
+router.post('api/burgers', (req, res) =>
+  burger.insertOne(req.body, result => res.json({ id: result.insertId }))
+);
+
+router.put('api/burgers/:id', (req, res) =>
+  burger.updateOne(req.params.id, 1, result =>
+    result.changedRows ? res.status(200).end() : res.status(404).end()
+  )
+);
 
 module.exports = router;
